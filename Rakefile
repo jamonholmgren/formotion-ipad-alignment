d13a7fdd4bfc93cfb1934a8d383b809df4df4c9b
Jamon Holmgren <jamon@clearsightstudio.com>
2013-05-16 10:39:41 -0700

    Added problematic form.

--- Rakefile
+++ Rakefile
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 $:.unshift("/Library/RubyMotion/lib")
 require 'motion/project/template/ios'
+require "bundler"
+Bundler.require
 
 Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.name = 'formotion-ipad-alignment'
+  app.device_family = [ :iphone, :ipad ]
 end
--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,27 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+
+    form = Formotion::Form.new({
+      sections: [{
+        title: "Label",
+        rows: [{
+          title: "",
+          name: "Textfield",
+          type: :text,
+          placeholder: "Textfield",
+          # row_height: 100
+        }]
+      }]
+    })
+
+    @view_controller = Formotion::FormController.alloc.initWithForm(form)
+    @view_controller.form.on_submit do |form|
+        p @view_controller.form.render
+      end
+
+    @window.rootViewController = @view_controller
+    @window.makeKeyAndVisible
     true
   end
 end
